9986e3d20e406fe608f5aaa87ba325ef7b9d056d
Carlos Rodriguez <carlosaudiovisual2015@gmail.com>
2021-03-09 11:45:03 -0500

    Proyect
    Migración de tabla proyectos

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -540,6 +540,7 @@ class ComposerStaticInitf6328520c809ab532feec759a7c3fbc7
         'App\\Http\\Middleware\\TrustHosts' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustHosts.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Models\\Proyect' => __DIR__ . '/../..' . '/app/Models/Proyect.php',
         'App\\Models\\User' => __DIR__ . '/../..' . '/app/Models/User.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
