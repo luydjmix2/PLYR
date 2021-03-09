53ece40b3968da6e49f21a903887f4f4b8594be1
Carlos Rodriguez <carlosaudiovisual2015@gmail.com>
2021-03-09 13:42:28 -0500

    Document
    Se crea el modelo y la migración  documentos

---
+++ database/migrations/2021_03_09_184016_create_documents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); //nombre
+            $table->unsignedInteger('proyectId'); //documentos
+            $table->foreign('proyectId')->references('id')->on('proyects');
+            $table->unsignedInteger('userId'); //documentos
+            $table->foreign('userId')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
