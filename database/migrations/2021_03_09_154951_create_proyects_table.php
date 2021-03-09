179f08baed140ffe4dd5d39f94702304d3c699bb
Carlos Rodriguez <carlosaudiovisual2015@gmail.com>
2021-03-09 11:45:03 -0500

    Proyect
    Migración de tabla proyectos

---
+++ database/migrations/2021_03_09_154951_create_proyects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProyectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proyects', function (Blueprint $table) {
+            $table->id();
+            $table->string('proyect_name'); //nombre proyecto
+            $table->unsignedBigInteger('userId'); //usuario asignado
+            $table->unsignedInteger('documentId'); //documentos
+
+            $table->foreign('userId')->references('id')->on('users');
+
+            $table->foreign('documentId')->references('id')->on('documents');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proyects');
+    }
+}
