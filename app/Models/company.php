e9926738064326d43137f9c776c06fab9e1ad224
luydjmix2 <luydjmix@hotmail.com>
2021-04-17 16:43:44 -0500

    asdasd

--- app/Models/company.php
+++ app/Models/company.php
@@ -5,7 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class company extends Model
-{
+class company extends Model {
+
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'company_name',
+        'company_bio',
+        'company_addres',
+        'company_phone',
+        'company_web',
+        'company_url_logo',
+        'company_code',
+        'company_nid',
+        'company_politics',
+        'user_id',
+    ];
+
 }
--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -3,16 +3,29 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\company;
 
-class CompanySeeder extends Seeder
-{
+class CompanySeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run() {
+
+        company::create([
+            'company_name' => 'MyCompania',
+            'company_bio' => 'demo Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s.',
+            'company_address' => 'streep demo 01',
+            'company_phone' => '1000000',
+            'company_web' => 'https//demo.com',
+            'company_url_logo' => '/media/various/ecom_product8.png',
+            'company_code' => '00010',
+            'company_nid' => '789420',
+            'company_politics' => 'yes',
+            'user_id' => '2',
+        ]);
     }
+
 }
